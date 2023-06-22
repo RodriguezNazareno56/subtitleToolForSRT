1929d63ff4c8499cc8752317eb0a0643623d8b37
Nazareno <nazarodriguez56@gmail.com>
2023-06-22 04:38:48 -0300

    docker-compose.yml with libreTranslate image and .sh modificated to up docker, generate .srt and get down docker

--- fromMultipleLinestoOneLine.sh
+++ fromMultipleLinestoOneLine.sh
@@ -1,6 +1,10 @@
-for i in 01 02 03 05 06
+docker compose up -d
+sleep 1m
+for i in 01 02 03
 do
-	python3 main.py "subs.S01E0$i.srt" "subs.S01E$i.oneLine.srt"
+	python3 main.py "Mad.Men.S01E$i.1080p.BluRay.x265-RARBG.srt" "Mad.Men.S01E$i.1080p.oneLine.srt"
 done
+docker compose down
+
 
 
